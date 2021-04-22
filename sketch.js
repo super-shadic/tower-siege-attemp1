53e9f4a7a23554b87635ee7445b82da3b2db8816
super-shadic <76892348+super-shadic@users.noreply.github.com>
2021-04-22 21:13:33 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var polygon, slingShot;
+var block1, block2, block3, block4, block5, block6, block7;
+var block8, block9, block10, block11, block12;
+var block13, block14, block15;
+var block16;
+
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  polygon = Bodies.circle(50,200,20);
+  World.add(world, polygon);
+
+  slingShot = new Slingshot(this.polygon, {x:100, y:200});
+
+// base layer
+
+  block1 = new Block(300, 275, 30, 40);
+  block2 = new Block(330, 275, 30, 40);
+  block3 = new Block(360, 275, 30, 40);
+  block4 = new Block(390, 275, 30, 40);
+  block5 = new Block(420, 275, 30, 40);
+  block6 = new Block(450, 275, 30, 40);
+  block7 = new Block(480, 275, 30, 40);
+
+// 2nd Layer
+
+  block8 = new Block(330, 235, 30, 40);
+  block9 = new Block(360, 235, 30, 40);
+  block10 = new Block(390, 235, 30, 40);
+  block11 = new Block(420, 235, 30, 40);
+  block12 = new Block(450, 235, 30, 40);
+
+// 3rd Layer
+
+  block13 = new Block(360, 195, 30, 40);
+  block14 = new Block(390, 195, 30, 40);
+  block15 = new Block(420, 195, 30, 40);
+
+// Top Layer
+
+  block16 = new Block(390, 155, 30, 40);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display();
+
+  drawSprites();
+}
